d5a767e0c55ef0f66756364796985f82b3757818
Damir_Kharakhursakh <damir_kharakhursakh@sluz.ch>
2025-09-30 13:10:23 +0200

    Aufgabe 14 fertig

---
+++ Aufgabe 14/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Aufgabe_14;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        bool keypressed = false;
+
+        while (keypressed == false)
+        {
+            string input;
+
+            Console.WriteLine("Geben Sie das Jahr ein: (q + enter to Quit)");
+            input = Console.ReadLine();
+            
+            if (input == "q")
+            {
+                keypressed = true; 
+                Console.WriteLine("You pressed q which stopped the programm");
+            }
+            else
+            {
+                int userJahr = Convert.ToInt32(input);
+                if (userJahr % 4 == 0)
+                {
+                    Console.WriteLine("Es ist ein Schaltjahr.");
+                }
+                else
+                {
+                    Console.WriteLine("Es ist KEIN Schaltjahr.");
+                }
+            }
+        }
+    }
+}
