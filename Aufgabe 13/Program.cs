38307a8ce938a113d6d3565e02453ada05eeb38c
Damir_Kharakhursakh <damir_kharakhursakh@sluz.ch>
2025-09-30 12:02:35 +0200

    Aufgabe 13 fertig

---
+++ Aufgabe 13/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Aufgabe_13;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Geben Sie die länge der Linie ein: ");
+        Console.WriteLine(" ");
+        string input = Console.ReadLine();
+        int intInput = Convert.ToInt32(input);
+        
+        string [,] stringArray = new string [intInput, intInput];
+
+        int x1 = 0;
+        int y1 = 0;
+        int x2 = 0;
+        int y2 = 0;
+
+        for (int i = 0; i < intInput; i++)
+        {
+            stringArray[i,i] = " ";
+        }
+        
+        for (int zeile = 0; zeile < stringArray.GetLength(0); zeile++)
+        {
+            for (int spalte = 0; spalte < stringArray.GetLength(1); spalte++)
+            {
+                Console.Write(stringArray[zeile, spalte] + "*");
+            }
+            Console.WriteLine();
+        }
+    }
+}
