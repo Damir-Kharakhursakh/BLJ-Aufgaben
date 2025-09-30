0c7333868d3d662a2cd57656f97af831f1107a56
Damir_Kharakhursakh <damir_kharakhursakh@sluz.ch>
2025-09-30 08:50:23 +0200

    Aufgabe 10 Fertig

---
+++ Aufgabe 10/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Aufgabe_10;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Gebe Sie eine Zahl ein: ");
+        Console.WriteLine(" ");
+        string input = Console.ReadLine();
+        int userNumber = Convert.ToInt32(input);
+        int sum = GetChecksum(userNumber);
+        Console.WriteLine(sum);
+    }
+
+    static int GetChecksum(int zahl) 
+    {
+        int sum = 0;
+        while (zahl != 0) 
+        {
+            sum += zahl % 10;
+            zahl /= 10;
+        }
+        return sum;
+    }
+}
